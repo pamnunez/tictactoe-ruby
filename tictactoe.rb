18887280191433eb452de72c7130a6048124aed9
spamela <nunezpam@gmail.com>
2014-03-10 17:36:14 -0400

    almost done with game mechanics. Changed board design, infinite turns, exit

--- tictactoe.rb
+++ tictactoe.rb
@@ -7,7 +7,7 @@ class TicTacToe
         @player = ""
         @gameend = 0
         @round = 1
-        @gameboard = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
+        @gameboard = Array.new(3, Array.new(3, " "))
         @input = ""
     end
 
@@ -15,13 +15,19 @@ class TicTacToe
         puts "Welcome to TicTacRuby!"
         puts "Please select your symbol. X goes first, O goes second. "
 
+        @player = gets.chomp
+        while @player !~ /[XO]/
+            puts "Invalid input! Please enter X or O: "
+            @player = gets.chomp
+        end 
+=begin
         begin
             @player = gets.chomp
             @player.match(/[XO]/)
         rescue
             puts "Invalid input! Please enter X or O: "
         end
-
+=end
         if @player == "X" then 
             puts "You will play as X, you will play first."
         else
@@ -30,46 +36,46 @@ class TicTacToe
         puts ""
     end
 
-
     def turnInit(board, roundnum)
         puts "Enter your symbol by indicating the position you prefer with the following numbers: "
-        puts "-------"
-        puts "|0|1|2|"
-        puts "-------"
-        puts "|3|4|5|"
-        puts "-------"
-        puts "|6|7|8|"
-        puts "-------"
+        puts ""
+        puts "0|1|2"
+        puts "-+-+-"
+        puts "3|4|5"
+        puts "-+-+-"
+        puts "6|7|8"
+        puts ""
         puts "You may quit the game at any time by entering \"exit\"."
         puts ""
         puts "Round #{roundnum}:"
-        puts "-------"
-        puts "|#{@gameboard[0][0]}|#{@gameboard[0][1]}|#{@gameboard[0][2]}|"
-        puts "-------"
-        puts "|#{@gameboard[1][0]}|#{@gameboard[1][1]}|#{@gameboard[1][2]}|"
-        puts "-------"
-        puts "|#{@gameboard[2][0]}|#{@gameboard[2][1]}|#{@gameboard[2][2]}|"
-        puts "-------"
+        puts ""
+        puts "#{@gameboard[0][0]}|#{@gameboard[0][1]}|#{@gameboard[0][2]}"
+        puts "-+-+-"
+        puts "#{@gameboard[1][0]}|#{@gameboard[1][1]}|#{@gameboard[1][2]}"
+        puts "-+-+-"
+        puts "#{@gameboard[2][0]}|#{@gameboard[2][1]}|#{@gameboard[2][2]}"
         puts ""
         puts "Please enter you choice: "
         @input = gets.chomp
         puts "You have entered #{@input}."
     end
 
-    def run
+    def run 
         if (gameend == 0) then
             turnInit(@gameboard, @round)
+        elsif (gameend == 1)
+            puts "Thank you for playing!"
+            exit
         end
     end
 
-#TODO: WRITE AI METHOD TO BE CALLED IN WHILE LOOP
+#TODO: WRITE AI FUNCTIONALITY. Once game mechanics are done, AI will be implemented.
 
 end
 
     game = TicTacToe.new
     game.gameBegin()
-    game.run
-        
+    game.run 
     
     gameend = 1
 
